d5bb984ee9405d55d0442186bd8009d3df6711a0
Mark Noizumi <mark.noizumi@gmail.com>
2017-07-12 13:22:26 -0700

    single letter check

--- scrabble.rb
+++ scrabble.rb
@@ -315,13 +315,13 @@ if __FILE__ == $PROGRAM_NAME
   p game
   p score #{:valid=>true, :score=>5}
 
-  tiles2 = [["m", 6, 13], ["y", 6, 14]]
+  tiles2 = [["m", 6, 13], ["i", 6, 14]]
   tiles2 = Scrabble.make_tiles(tiles2)
   score = game.play_tiles(tiles2)
   p game
   p score
 
-  tiles3 = [["s",7,14]]
+  tiles3 = [["s",7,14], ["t",8,14]]
   tiles3 = Scrabble.make_tiles(tiles3)
   score = game.play_tiles(tiles3)
   p game
